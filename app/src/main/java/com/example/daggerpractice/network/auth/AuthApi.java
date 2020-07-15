550e42e1001516ff9ce06f09f007d808fa2f8c08
seyed mahdi <ssmmhh1382@gmail.com>
2020-07-15 17:20:01 +0100

    16-Retrofit calls and RxJava

--- app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.java
+++ app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.java
@@ -1,10 +1,17 @@
 package com.example.daggerpractice.network.auth;
 
+import com.example.daggerpractice.models.User;
+import com.google.gson.annotations.Expose;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUserById(
+            @Path("id") int id
+    );
 }
--- app/src/main/java/com/example/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/example/daggerpractice/ui/auth/AuthViewModel.java
@@ -2,11 +2,15 @@ package com.example.daggerpractice.ui.auth;
 
 import android.util.Log;
 
+import com.example.daggerpractice.models.User;
 import com.example.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
 import androidx.lifecycle.ViewModel;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
@@ -14,9 +18,30 @@ public class AuthViewModel extends ViewModel {
     @Inject
     public AuthViewModel(AuthApi authApi) {
         Log.d(TAG, "AuthViewModel: viewModel is working yea...");
-        if (authApi==null){
-            Log.e(TAG, "AuthViewModel: auth api is null" );
-        }else
-            Log.d(TAG, "AuthViewModel: auth api is not NULL");
+
+        authApi.getUserById(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG, "onNext: "+user.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: ",e );
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
