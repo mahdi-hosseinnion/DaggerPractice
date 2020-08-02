ae21e77f88cf4d2abdccd6b4e7afbd1b5ccfb624
seyed mahdi <ssmmhh1382@gmail.com>
2020-08-02 18:41:57 +0100

    32-NavController and navigation graph

--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.daggerpractice.ui.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,13 +15,18 @@ import com.example.daggerpractice.BaseActivity;
 import com.example.daggerpractice.R;
 import com.example.daggerpractice.ui.main.posts.PostsFragment;
 import com.example.daggerpractice.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
-
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //ui component
+    DrawerLayout drawer_layout;
+    NavigationView nav_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawer_layout=findViewById(R.id.drawer_layout);
+        nav_view=findViewById(R.id.nav_view);
         testFragment();
     }
     private void testFragment(){
@@ -43,5 +50,20 @@ public class MainActivity extends BaseActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.profile_menu:{
+
+            }
+            case R.id.posts_menu:{
+
+            }
+        }
+        menuItem.setChecked(true);
+        drawer_layout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
